7c88c92a1016130f33ac7f0629bc0ff9af701497
vanquish1905 <c@m>
2026-08-18 11:15:32 +0200

    added more fetatures

--- src/ArrayTools.java
+++ src/ArrayTools.java
@@ -1,5 +1,7 @@
 public class ArrayTools {
-
+    private static float[] arg = {3.544f, 5.544f, 6.544f, 6.544f};
+    private static int[] argi = {3, 5, 6, 6};
+    private static String[] args = {"a","b","c","d"};
     public static int head(int[] array){
         return array[0];
     }
@@ -14,8 +16,43 @@ public class ArrayTools {
         arrays = arrays +"]";
         return arrays;
     }
+    public static String arrayToString(char[] array){
+        String arrays = "[";
+        for (char number : array){
+            arrays = arrays + number +", ";
+        }
+        arrays = arrays +"]";
+        return arrays;
+    }
+    public static String arrayToString(float[] array){
+        String arrays = "[";
+        for (float number : array){
+            arrays = arrays + number +", ";
+        }
+        arrays = arrays +"]";
+        System.out.println(arrays);
+        return arrays;
+    }
+    public static int[] copy(int[] array){
+        int[] arrays = new int[array.length];
+        for(int i=0; i<array.length;i++){
+            arrays[i] = array[i];
+        }
+        return arrays;
+    }
+    public static int[] copy(int[] array, int start, int end){
+        int[] arrays = new int[end];
+        for(int i=start; i<end;i++){
+            arrays[i] = array[i];
+        }
+        System.out.println(arrayToString(arrays));
+        return arrays;
+    }
 
 
+    public static void main(String[] args) {
+        copy(argi,1,4);
+    }
 
 
 }
